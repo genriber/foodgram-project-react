e7010d55cac7a77e85aac7bb793908cb8ceea0ed
Алексей Каргаев <genriber@gmail.com>
2023-06-18 20:49:05 +0400

    switched back to sqlite

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = "foodgram.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-
+"""
 DATABASES = {
     "default": {
         "ENGINE": os.getenv(
@@ -102,7 +102,7 @@ DATABASES = {
         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
-"""
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
